8d2339224a05d62a75bafa56e93f91f8b9a70607
jeshin <jsshin0201@gmail.com>
2024-09-23 18:12:22 +0900

    go test

---
+++ srcs/mariadb/mdb_conf.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# mariadb start
+service mariadb start
+sleep 5
+
+# mariadb config
+# create database if not exists
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB}\`;"
+
+# create user if not exists
+mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+
+#grant privilegs to user
+mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO \`${MYSQL_USER}\`@'%';"
+
+#flush privileges to apply changes
+mariadb -e "FLUSH PRIVILEGES;"
+
+#mariadb restart
+# shutdown mariadb to restart with new config
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+
+#restart mariadb with new config in the background to keep the container running
+mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
